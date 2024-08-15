23968bdbb28f0b0846ef6947017fce21c210849b
matthew-gigl-db <matthew-gigl-db>
2024-08-15 02:47:11 +0000

    tags npi

--- src/apply_tags_npi.sql
+++ src/apply_tags_npi.sql
@@ -1,5 +1,22 @@
 -- Databricks notebook source
 DECLARE OR REPLACE VARIABLE catalog_use STRING;
+DECLARE OR REPLACE VARIABLE tableName STRING;
 
 SET VAR catalog_use = :`bundle.catalog`; 
-SELECT catalog_use;
+SET VAR tableName = :table;
+
+SELECT catalog_use, tableName;
+
+-- COMMAND ----------
+
+DECLARE OR REPLACE VARIABLE tag_statement STRING;
+
+SET VAR tag_statement = "alter table IDENTIFIER(catalog_use || '.hv_claims.' || tableName) alter column npi set tags ('pii' = 'true');"
+
+-- COMMAND ----------
+
+EXECUTE IMMEDIATE tag_statement;
+
+-- COMMAND ----------
+
+EXECUTE IMMEDIATE "select * from IDENTIFIER(catalog_use || '.information_schema.column_tags') where schema_name = 'hv_claims'";
