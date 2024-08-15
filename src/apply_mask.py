cae362471ddf567b9f9ba46f6ce36ae4c9b4615d
matthew-gigl-db <matthew-gigl-db>
2024-08-15 05:45:03 +0000

    add some titles

--- src/apply_mask.py
+++ src/apply_mask.py
@@ -1,18 +1,22 @@
 # Databricks notebook source
+# DBTITLE 1,Creating a Text Input Widget in Databricks
 dbutils.widgets.text("bundle.catalog", "")
 
 # COMMAND ----------
 
+# DBTITLE 1,Accessing Database Widgets in Python Code
 catalog_use = dbutils.widgets.get("bundle.catalog")
 
 # COMMAND ----------
 
+# DBTITLE 1,Switching Catalog and Schema in PySpark Queries
 spark.sql(f"USE CATALOG {catalog_use}")
 spark.sql("use schema hv_claims")
 display(spark.sql("select current_catalog(), current_schema()"))
 
 # COMMAND ----------
 
+# DBTITLE 1,Pulling the Tagged Columns from the hv_claims schema
 sqlStmt = f"""
   SELECT * FROM information_schema.column_tags where schema_name = 'hv_claims'
 """
@@ -22,6 +26,7 @@ display(tagged_columns)
 
 # COMMAND ----------
 
+# DBTITLE 1,Apply Masks to Columns Tagged with PiI = True
 # Iterate over each row in the DataFrame
 for row in tagged_columns.collect():
     stmnt = f"""ALTER TABLE {row.catalog_name}.{row.schema_name}.{row.table_name} 
@@ -32,6 +37,7 @@ for row in tagged_columns.collect():
 
 # COMMAND ----------
 
+# DBTITLE 1,Show Masks
 # MAGIC %sql
 # MAGIC
 # MAGIC select * from information_schema.column_masks
--- src/apply_tags_npi.sql
+++ src/apply_tags_npi.sql
@@ -1,4 +1,5 @@
 -- Databricks notebook source
+-- DBTITLE 1,Variable Declaration and Assignment in SQL
 DECLARE OR REPLACE VARIABLE catalog_use STRING;
 DECLARE OR REPLACE VARIABLE tableName STRING;
 
@@ -9,14 +10,17 @@ SELECT catalog_use, tableName;
 
 -- COMMAND ----------
 
+-- DBTITLE 1,Tagging Columns for Privacy in SQL Queries
 DECLARE OR REPLACE VARIABLE tag_statement STRING;
 
 SET VAR tag_statement = "alter table IDENTIFIER(catalog_use || '.hv_claims.' || tableName) alter column npi set tags ('pii' = 'true');"
 
 -- COMMAND ----------
 
+-- DBTITLE 1,Executing Immediate SQL Commands Dynamically
 EXECUTE IMMEDIATE tag_statement;
 
 -- COMMAND ----------
 
+-- DBTITLE 1,Return tagged columns
 EXECUTE IMMEDIATE "select * from IDENTIFIER(catalog_use || '.information_schema.column_tags') where schema_name = 'hv_claims'";
