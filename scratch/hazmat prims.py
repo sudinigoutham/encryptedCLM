ebe4a3b59823d83f46444b8b238b5650876f3888
matthew-gigl-db <matthew-gigl-db>
2024-08-19 02:07:30 +0000

    hazmat with fernet generated key

--- scratch/hazmat prims.py
+++ scratch/hazmat prims.py
@@ -1,4 +1,14 @@
 # Databricks notebook source
+# MAGIC %md
+# MAGIC
+# MAGIC A basic example of an encryption method that returns the same string every time, using the unicode code point of each one character string rising to the key value and put back together.  The encryption and decrpytion functions are technically the same.  
+
+# COMMAND ----------
+
+key = 327
+
+# COMMAND ----------
+
 # super basic encryption/decryption
 def encrypt_message(message, key):
     encrypted_message = ""
@@ -13,6 +23,19 @@ def decrypt_message(encrypted_message, key):
         decrypted_char = chr(ord(char) ^ key)
         decrypted_message += decrypted_char
     return decrypted_message
+
+# COMMAND ----------
+
+encrypt_message("Spark is awesome", key)
+
+# COMMAND ----------
+
+decrypt_message('ĔķĦĵĬŧĮĴŧĦİĢĴĨĪĢ', key)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC This version uses the crpyotoraphy package's hazmat classes and methods for creating a Cipher with a key and returns the same string each time.  The key used it still an integer turned to bytes.  
 
 # COMMAND ----------
 
@@ -40,12 +63,109 @@ def decrypt_message(encrypted_message, key):
 
 # COMMAND ----------
 
-encrypt_message("Spark is awesome", 5)
+encrypt_message("Spark is awesome", key)
 
 # COMMAND ----------
 
-encrypt_message("Spark is awesome", 5)
+decrypt_message('b0a74079c722720d676fdcd84c0ba3378601552a448929f26ee69e7e55e507dd', key)
 
 # COMMAND ----------
 
-decrypt_message('6b382772e73e622dcfedb1ed70c486f526912f568ee4cf1e45fa847a8b0cde67', 5)
+# MAGIC %md
+# MAGIC
+# MAGIC We can make the key more complicated by using the same method as the Fernet class, and supply a salt that is truly random using the os.urandom method.  If the key and the salt are stored and used the same, then the value returned from the encrpytion function will always be the same, otherwise if we allow the salt to vary we can have different encrpyted strings, but the key will decrypt the message.  
+
+# COMMAND ----------
+
+from cryptography.fernet import Fernet
+
+# COMMAND ----------
+
+key = Fernet.generate_key().decode()
+
+# COMMAND ----------
+
+from cryptography.hazmat.primitives import padding
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+import base64
+import os
+
+# COMMAND ----------
+
+salt = os.urandom(16)
+
+# COMMAND ----------
+
+salt
+
+# COMMAND ----------
+
+salt_hex = salt.hex()
+
+# COMMAND ----------
+
+salt_hex
+
+# COMMAND ----------
+
+salt_bytes = bytes.fromhex(salt_hex)
+
+# COMMAND ----------
+
+salt_bytes == salt
+
+# COMMAND ----------
+
+def get_key_bytes(key_str, salt):
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=100000,
+        backend=default_backend()
+    )
+    key = base64.urlsafe_b64decode(key_str)
+    return kdf.derive(key)
+
+def encrypt_message(message, key_str, salt):
+    key_bytes = get_key_bytes(key_str, salt)
+    backend = default_backend()
+    cipher = Cipher(algorithms.AES(key_bytes), modes.ECB(), backend=backend)
+    encryptor = cipher.encryptor()
+    padder = padding.PKCS7(128).padder()
+    padded_data = padder.update(message.encode()) + padder.finalize()
+    encrypted_message = encryptor.update(padded_data) + encryptor.finalize()
+    return base64.urlsafe_b64encode(salt + encrypted_message).decode()
+
+def decrypt_message(encrypted_message, key_str):
+    encrypted_message_bytes = base64.urlsafe_b64decode(encrypted_message)
+    salt = encrypted_message_bytes[:16]
+    encrypted_message_bytes = encrypted_message_bytes[16:]
+    key_bytes = get_key_bytes(key_str, salt)
+    backend = default_backend()
+    cipher = Cipher(algorithms.AES(key_bytes), modes.ECB(), backend=backend)
+    decryptor = cipher.decryptor()
+    decrypted_padded_message = decryptor.update(encrypted_message_bytes) + decryptor.finalize()
+    unpadder = padding.PKCS7(128).unpadder()
+    decrypted_message = unpadder.update(decrypted_padded_message) + unpadder.finalize()
+    return decrypted_message.decode()
+
+# COMMAND ----------
+
+encrypt_message("Spark is awesome", key, salt)
+
+# COMMAND ----------
+
+decrypt_message(encrypt_message("Spark is awesome", key, salt), key)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC What about Null values?
+
+# COMMAND ----------
+
+encrypt_message(None, key, salt)
