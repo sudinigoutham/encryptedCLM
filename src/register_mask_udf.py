6db02d6546dea50f81e2599f054e2cc8bedfcb7d
matthew-gigl-db <matthew-gigl-db>
2024-08-19 03:20:12 +0000

    updated encrpytion mask function and added decryption mask style

--- src/register_mask_udf.py
+++ src/register_mask_udf.py
@@ -1,33 +1,47 @@
 # Databricks notebook source
+# DBTITLE 1,Creating Text Widgets with DBUtils in Python
 dbutils.widgets.text("bundle.catalog", "")
 
 # COMMAND ----------
 
+# DBTITLE 1,Accessing Bundle Catalog with DBUtils in Python
 catalog_use = dbutils.widgets.get("bundle.catalog")
 
 # COMMAND ----------
 
+# DBTITLE 1,Switching Database Contexts in Spark SQL Queries
 spark.sql(f"USE CATALOG {catalog_use}")
 spark.sql("use schema hv_claims")
 display(spark.sql("select current_catalog(), current_schema()"))
 
 # COMMAND ----------
 
+# DBTITLE 1,Creating an Encryption Mask Function in SQL
 function_stmnt = f"""
   CREATE OR REPLACE FUNCTION encryption_mask(input_text STRING)
   RETURN CASE
-    when IS_MEMBER('encryptionCLM_demo_mask') then {catalog_use}.hv_claims.encrypt_text(input_text, secret('encryptionCLM-demo', 'encryption_key'))
-    else input_text 
+    when IS_MEMBER('encryptionCLM_demo_mask') then input_text
+    else {catalog_use}.hv_claims.encrypt_text(input_text, secret('encryptionCLM-demo', 'encryption_key'), secret('encryptionCLM-demo', 'encryption_salt')) 
   END;
 """
 
 # COMMAND ----------
 
+# DBTITLE 1,Executing SQL Commands with Spark in Python
 spark.sql(function_stmnt)
 
 # COMMAND ----------
 
-# %sql
+# DBTITLE 1,SQL Function for Conditional Text Decryption
+function_stmnt = f"""
+  CREATE OR REPLACE FUNCTION decryption_mask(input_text STRING)
+  RETURN CASE
+    when IS_MEMBER('encryptionCLM_demo_mask') then {catalog_use}.hv_claims.decrypt_text(input_text, secret('encryptionCLM-demo', 'encryption_key'), secret('encryptionCLM-demo', 'encryption_salt')) 
+    else input_text
+  END;
+"""
 
-# select encryption_mask('hello'), decrypt_text(encryption_mask('hello'), secret('encryptionCLM-demo', 'encryption_key'))
-# ; 
+# COMMAND ----------
+
+# DBTITLE 1,Executing SQL Queries with Spark
+spark.sql(function_stmnt)
